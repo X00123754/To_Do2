3849d4e44cc8eeaadc237fc41f330f49fdb7ae7a
Enda Lee <enda.lee@ittdublin.ie>
2016-11-30 15:43:53 +0000

    Starting lab 11

---
+++ wk1_lab1/assets/js/game.js
@@ -0,0 +1,13 @@
+﻿// This file contains JavaScript Functions which are used in the site
+
+
+// Function name:	SetCookie(strCookieName, strCookieValue, intLifeTimesDays)
+// Description:		Set a cookie assigning value to name with expiry date today + number of days
+// Parameters:		min		Start of number range
+//					max 	End of number range
+// Return			integer value
+
+function getRandomInt(min, max)
+{ 
+	return Math.floor(Math.random() * (max - min + 1)) + min; 
+} 
